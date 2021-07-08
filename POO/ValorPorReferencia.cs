1f3e06f5513b283fed308573504717a6ca35fe56
Davisilveira <80595329+Unordestino@users.noreply.github.com>
2021-07-08 15:38:04 -0300

    kyukuyuk
    uyukyukyu

--- POO/ValorPorReferencia.cs
+++ POO/ValorPorReferencia.cs
@@ -1,1 +1,15 @@
 
+    dD
+
+
+        static void Main(string[] args)
+        {
+            int a = 3;
+            AlterarRef(ref a);
+            Console.WriteLine(a);
+
+            int b = 2;
+            AlterarOut(ref b);
+            Console.WriteLine(b);
+
+        }
